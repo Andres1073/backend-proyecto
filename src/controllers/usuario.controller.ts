f6bb47239606d9ac2b1808969118e6c9e12feb58
unknown <andresking17@gmail.com>
2021-11-24 14:58:56 -0500

    Backend a jwt

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,30 +1,29 @@
+import {service} from '@loopback/core';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
 import {Usuario} from '../models';
 import {UsuarioRepository} from '../repositories';
+import {AutenticacionService} from '../services';
+const fetch = require("node-fetch");
 
 export class UsuarioController {
   constructor(
     @repository(UsuarioRepository)
-    public usuarioRepository : UsuarioRepository,
-  ) {}
+    public usuarioRepository: UsuarioRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion: AutenticacionService
+  ) { }
 
   @post('/usuarios')
   @response(200, {
@@ -37,14 +36,30 @@ export class UsuarioController {
         'application/json': {
           schema: getModelSchemaRef(Usuario, {
             title: 'NewUsuario',
-            
+
           }),
         },
       },
     })
     usuario: Usuario,
   ): Promise<Usuario> {
-    return this.usuarioRepository.create(usuario);
+
+    let clave = this.servicioAutenticacion.GenerarClave();
+    let claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    usuario.clave = claveCifrada;
+    let p = await this.usuarioRepository.create(usuario);
+
+    // notificacion de usuario
+
+    let destino = usuario.correo;
+    let asunto = 'Registro en la plataforma';
+    let contenido = `Hola ${usuario.nombres}, su nombre de usuario es: ${usuario.correo}, y su contraseña es: ${clave}`;
+
+    fetch(`http://127.0.0.1:5000/envio-correo?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+      .then((data: any) => {
+        console.log(data);
+      })
+    return p;
   }
 
   @get('/usuarios/count')
